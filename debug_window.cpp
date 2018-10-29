47962d7961a08e58947bc0cad041d0ef75a9246d
TOGURO Mikito <mit@shalab.net>
2018-10-30 01:07:58 +0900

    DebugWindow の導入
    Debug 用の デバッグコンソールウィンドウの導入開始

---
+++ debug_window.cpp
@@ -0,0 +1,29 @@
+﻿#include <tchar.h>
+#include <Windows.h>
+#include <thread>
+#include "debug_window.h"
+
+namespace implement{
+  static std::atomic<HWND>& getDebugWindowHandleImpl();
+};
+
+void bootstrap_debug_console( std::thread& application_thread )
+{
+  HANDLE const app_thread_handle = application_thread.native_handle();
+
+  (void)( app_thread_handle );
+  
+  return;
+}
+
+static std::atomic<HWND>& implement::getDebugWindowHandleImpl()
+{
+  static std::atomic<HWND> debugWindowHandle{nullptr};
+  return debugWindowHandle;
+}
+
+HWND getDebugWindowHandle()
+{
+  HWND hWnd = implement::getDebugWindowHandleImpl().load();
+  return hWnd;
+}
---
+++ debug_window.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#if !defined( DEBUG_WINDOW_H_HEADER_GUARD30fb1f5c_3d07_498c_bcbd_13bc46b7886b )
+#define DEBUG_WINDOW_H_HEADER_GUARD30fb1f5c_3d07_498c_bcbd_13bc46b7886b 1
+
+#include <atomic>
+
+#if defined( __cplusplus )
+#define EXTERN_C extern "C" 
+#else /* defined( __cplusplus ) */
+#define EXTERN_C 
+#endif /* defined( __cplusplus ) */
+
+EXTERN_C HWND getDebugWindowHandle();
+
+#if defined( __cplusplus )
+void bootstrap_debug_console( std::thread& application_thread );
+#endif /* defined( __cplusplus )  */
+
+#endif /* DEBUG_WINDOW_H_HEADER_GUARD30fb1f5c_3d07_498c_bcbd_13bc46b7886b */
